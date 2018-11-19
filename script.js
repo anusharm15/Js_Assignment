2061577def9267b805b4f34c48d3a10b9151328f
Anuj Sharma <anujsharma@Anujs-MacBook-Air.local>
2018-11-19 11:59:31 +0530

    added new branch

--- script.js
+++
@@ -1,7 +0,0 @@
-import './scss/styles.scss';
-import './scss/loader.scss';
-import {controller} from './modules/render';
-
-window.addEventListener("DOMContentLoaded",function(){
-	controller.loadData()
-})
