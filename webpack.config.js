2ff100b27feb5238738621ce072730e59d7d3f7c
Anuj Sharma <anujsharma@Anujs-MacBook-Air.local>
2018-11-21 10:05:38 +0530

    added code for dist file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 var webpack = require('webpack');
+const path = require('path')
 
 module.exports = {
 	entry: ['./src/script.js'],
 	output: {
-		filename: 'bundle.js'
+		path: path.resolve(__dirname, 'dist'),
+    	publicPath: '/dist/',
+    	filename: 'bundle.js'
 	},
 	devtool: 'sourcemap',
 	module: {
